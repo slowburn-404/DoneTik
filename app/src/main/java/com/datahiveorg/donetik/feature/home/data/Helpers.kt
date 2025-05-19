147a5741088b40261ec35a48c389a882f927980a
slowburn-404 <borisochola32@gmail.com>
2025-05-19 23:20:28 +0300

    refactored firebase auth service to return a firebase user instead of auth result

---
+++ app/src/main/java/com/datahiveorg/donetik/feature/home/data/Helpers.kt
@@ -0,0 +1,76 @@
+package com.datahiveorg.donetik.feature.home.data
+
+
+import com.datahiveorg.donetik.feature.auth.domain.model.User
+import com.datahiveorg.donetik.feature.home.domain.model.Task
+import com.datahiveorg.donetik.firebase.model.FirebaseRequest.TaskDTO
+import com.datahiveorg.donetik.firebase.model.FirebaseRequest.UserDTO
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+fun TaskDTO.toHomeDomain(): Task {
+    return Task(
+        id = id,
+        author = author.toHomeDomain(),
+        title = title,
+        description = description,
+        isDone = isDone,
+        createdAt = createdAt.toDate().toHomeDomain(),
+        lastModified = lastModified.toDate().toHomeDomain()
+    )
+}
+
+fun Throwable.toHomeDomain(): String {
+    return this.message.toString()
+}
+
+fun Task.toFireBase(): Map<String, Any> = mapOf(
+    "id" to id,
+    "author" to author.toUserDTO(),
+    "title" to title,
+    "description" to description,
+    "isDone" to isDone,
+    "createdAt" to createdAt.toFireStoreTimeStamp(),
+    "lastModified" to lastModified.toFireStoreTimeStamp()
+
+)
+
+fun User.toUserDTO(): Map<String, Any> {
+    return mapOf(
+        "uid" to uid,
+        "username" to username,
+        "email" to email,
+        "imageUrl" to imageUrl
+    )
+}
+
+fun UserDTO.toHomeDomain(): User {
+    return User(
+        uid = uid,
+        username = username,
+        email = email,
+        imageUrl = imageUrl,
+        password = ""
+    )
+}
+
+
+//TODO: Move to a more common place
+fun Date.toHomeDomain(): String {
+    val sdf = SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault())
+    sdf.timeZone = TimeZone.getDefault()
+    return sdf.format(this)
+}
+
+fun String.toDate(): Date {
+    val sdf = SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault())
+    sdf.timeZone = TimeZone.getTimeZone("UTC")
+    return sdf.parse(this)!! // TODO: remember to handle null dates
+}
+
+fun String.toFireStoreTimeStamp(): Timestamp {
+    return Timestamp(this.toDate())
+}
